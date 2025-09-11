0ec92bcddadb4675ae31c62e447531bd64f69a4b
Kalpesh-9850 <narkhedekalpesh9850@gmail.com>
2025-09-11 12:10:03 +0530

    img added

--- script.js/Access_api.js
+++ script.js/Access_api.js
@@ -1,3 +1,3 @@
 function accessApi(){
-    alert("✅ API copied to clipboard!\n\n📌 This API key is available for 7 days.");
+    alert("✅ API copied to clipboard!\n\n📌 This API key is available for 7 days.  ");
 }
